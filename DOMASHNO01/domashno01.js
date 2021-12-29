da25d0763a1bdeaef4dbf20ee7bedf36d38ecf60
milenalazovska <96383916+milenalazovska@users.noreply.github.com>
2021-12-29 14:26:05 +0100

    Delete domashno01.js

--- DOMASHNO01/domashno01.js
+++
@@ -1,9 +0,0 @@
-var a = 119.95;
-var b = 30;
-var price = (b * a);
-console.log(`${b} phones are ${price} $`);
-var tax = 0.05;
-var pricetax = price * tax;
-console.log (`Price with tax ${pricetax}$`);
-finallyprice = pricetax + price;
-console.log(`The finally price is ${finallyprice}$`);
