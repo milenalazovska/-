479b4c93f956ed68c4de1c79a80e9079499ff9c3
milenalazovska <96383916+milenalazovska@users.noreply.github.com>
2021-12-29 14:27:45 +0100

    Add files via upload

---
+++ DOMASHNOI-01/domashno01.js
@@ -0,0 +1,9 @@
+var a = 119.95;
+var b = 30;
+var price = (b * a);
+console.log(`${b} phones are ${price} $`);
+var tax = 0.05;
+var pricetax = price * tax;
+console.log (`Price with tax ${pricetax}$`);
+finallyprice = pricetax + price;
+console.log(`The finally price is ${finallyprice}$`);
