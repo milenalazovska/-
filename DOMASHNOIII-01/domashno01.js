34e6d6b9727b4b2b6eda0b1e36258c472682f7e5
milenalazovska <96383916+milenalazovska@users.noreply.github.com>
2022-01-08 13:03:18 +0100

    Add files via upload

---
+++ DOMASHNOIII-01/domashno01.js
@@ -0,0 +1,17 @@
+let input1 = 1
+let input2 = "Milena"
+let input3 = false
+let input4  
+let input5 = {}
+
+function getInputType(input){
+    let type = typeof input
+        return type
+}
+console.log(getInputType(input1))
+console.log(getInputType(input2))
+console.log(getInputType(input3))
+console.log(getInputType(input4))
+console.log(getInputType(input5))
+
+
