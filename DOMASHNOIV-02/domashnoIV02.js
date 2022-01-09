0154ceb00d299a947b459e2a96789d83c454d158
milenalazovska <96383916+milenalazovska@users.noreply.github.com>
2022-01-09 13:53:57 +0100

    Add files via upload

---
+++ DOMASHNOIV-02/domashnoIV02.js
@@ -0,0 +1,6 @@
+function sum(number01, number02, number03, number04, number05){
+    return number01 + number02 + number03 + number04 + number05
+}
+
+let arrayNumbers = [5, 7, 8, 3, 10]
+console.log(sum(arrayNumbers[0],arrayNumbers[1],arrayNumbers[2],arrayNumbers[3],arrayNumbers[4]))
