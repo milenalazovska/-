2d095d39dce550bbb766a11ff76834d60ba3ad75
milenalazovska <96383916+milenalazovska@users.noreply.github.com>
2022-01-09 13:54:39 +0100

    Add files via upload

---
+++ DOMASHNOIV-03/domashnoIV03.js
@@ -0,0 +1,17 @@
+function quotes(word1, word2, word3, word4, word5){
+  return `Milena said: ${word1} ${word2} ${word3} ${word4} ${word5}`;
+}
+
+ let myArrayQuote = ['Hello', 'there', 'colleagues', 'of', 'G4!'];
+ console.log(quotes(myArrayQuote[0],myArrayQuote[1],myArrayQuote[2],myArrayQuote[3],myArrayQuote[4]));
+
+
+
+
+
+
+
+
+
+
+
